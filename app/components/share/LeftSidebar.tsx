d5d848e0b0e600c9bc1151792dedc0fd34b08b66
k-i-r-o-o <raunakc6@gmail.com>
2023-10-21 20:00:48 +0530

    final

--- app/components/share/LeftSidebar.tsx
+++ app/components/share/LeftSidebar.tsx
@@ -1,7 +1,14 @@
 
 function LeftSideBar()
 {
-    return <h1>LeftSideBar</h1>
+    return
+(
+    <section className="custom-scrollbar leftsideebar">
+        <div className="flex w-full flex-1 flex-col gap-6 px-6">
+            
+        </div>
+    </section>
+)
 }
 
 export default LeftSideBar;
