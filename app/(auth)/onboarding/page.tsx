1e1aba14b11434944b790099828ef1ef7b28738a
k-i-r-o-o <raunakc6@gmail.com>
2023-10-21 19:58:40 +0530

    working

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,10 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
 async function Page()
 {
     return(
         <main>
             <h1 className="head-text">Onboarding</h1>
+            <OrganizationSwitcher/>
         </main>
     )
 }
